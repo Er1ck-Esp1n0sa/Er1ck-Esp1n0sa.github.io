763e1277344478f06d9674fd64e90f9c4e0a854b
erick <erikc_48@hotmail.com>
2022-09-09 10:58:58 -0500

    Create branch lenguajes #12

--- src/app/skill-and-competences/skill-and-competences.component.ts
+++ src/app/skill-and-competences/skill-and-competences.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillAndCompetencesComponent implements OnInit {
 
+  skill_c : Array<any> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    let hb1 = {
+      Nom_skill : "Habilidad para aprender rapido",
+      Des_skill : [
+        {Descripccion : "✨✨✨✨✨"}
+      ]
+};
+let hb2 = {
+  Nom_skill : "Habilidad para comunicarse con las personas",
+  Des_skill : [
+    {Descripccion : "✨✨✨✨"}
+  ]
+};
+let hb3 = {
+  Nom_skill : "Habilidad de programador",
+  Des_skill : [
+    {Descripccion : "✨✨✨"}
+  ]
+};
+let hb4 = {
+  Nom_skill : "Habilidad de administrador",
+  Des_skill : [
+    {Descripccion : "✨✨✨"}
+  ]
+};
+
+
+
+this.skill_c.push(hb1);
+this.skill_c.push(hb2);
+this.skill_c.push(hb3);
+this.skill_c.push(hb4);
   }
 
 }
